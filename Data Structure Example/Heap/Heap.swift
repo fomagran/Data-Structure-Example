12b80df8fc82c6a1fc7f3afa3d9a7ea829605342
Fomagran <fomagran6@naver.com>
2021-10-18 18:47:11 +0900

    [add] MaxHeap 삭제 구현해보기 아직 완성전

--- Data Structure Example/Heap/Heap.swift
+++ Data Structure Example/Heap/Heap.swift
@@ -7,6 +7,57 @@
 
 import Foundation
 
-class Heap {
+struct MaxHeap {
+    var nodes:[Int] = []
     
+    init(nodes:[Int]) {
+        self.nodes = nodes
+    }
+    
+    private func getLeftChildIndex(_ parentIndex: Int) -> Int {
+        return 2 * parentIndex + 1
+    }
+    private func getRightChildIndex(_ parentIndex: Int) -> Int {
+        return 2 * parentIndex + 2
+    }
+    private func getParentIndex(_ childIndex: Int) -> Int {
+        return (childIndex - 1) / 2
+    }
+    
+    // Boolean Check
+    private func hasLeftChild(_ index: Int) -> Bool {
+        return getLeftChildIndex(index) < nodes.count
+    }
+    private func hasRightChild(_ index: Int) -> Bool {
+        return getRightChildIndex(index) < nodes.count
+    }
+    private func hasParent(_ index: Int) -> Bool {
+        return getParentIndex(index) >= 0
+    }
+    
+    mutating func remove()  {
+        if nodes.isEmpty {
+            return
+        }
+        nodes.swapAt(0, nodes.count - 1)
+        heapifyDown(from: 0)
+        nodes.removeLast()
+    }
+    
+    mutating func heapifyDown(from index:Int) {
+        var index = 0
+        while hasLeftChild(index) {
+            var biggerChildIndex = getLeftChildIndex(index)
+            if hasRightChild(index){
+                biggerChildIndex = getRightChildIndex(index)
+            }
+            
+            if nodes[index] < nodes[biggerChildIndex] {
+                break
+            } else {
+                nodes.swapAt(index, biggerChildIndex)
+            }
+            index = biggerChildIndex
+        }
+    }
 }
--- Data Structure Example/ViewController.swift
+++ Data Structure Example/ViewController.swift
@@ -12,30 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let head = Node(value: 0)
-        let list = LinkedList(head: head)
-
-        list.append(value: 1)
-        list.append(value: 2)
-        list.append(value: 3)
+        var heap = MaxHeap(nodes: [5,4,3,1,2,1,2])
+        heap.remove()
+        print(heap.nodes)
         
-        list.insert(value: 4, at: 0)
-        list.insert(value: 5, at: 1)
-        list.insert(value: 10, at: 4)
-        
-        //45011023
-        
-        list.remove(at: 1)
-        list.remove(at: 1)
-        list.remove(at: 2)
-        
-        //4123
-        
-        var node:Node? = list.head
-        print(list.head.value)
-        while node?.next != nil {
-            print(node!.next!.value)
-            node = node?.next
-        }
     }
 }
